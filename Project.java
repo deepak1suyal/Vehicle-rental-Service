25fb7107d2bdf3ba32620ae7b6b2aad7124ac9d8
deepak1suyal <148374034+deepak1suyal@users.noreply.github.com>
2023-11-23 18:00:23 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -15,6 +15,8 @@ public class Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        apps.login l=new apps.login();
+        l.setVisible(true);
     }
     
 }
